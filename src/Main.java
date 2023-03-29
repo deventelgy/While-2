1ca47290023da44edb29d66bcb4e3342c7d45510
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-29 09:51:39 -0500

    Uso de While parte 2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int num=1;
+
+        //Definimos el bucle, incluye el 100
+        while (num<=100){
+            if (num%2==0 || num%3==0){
+                System.out.println(num);
+            }
+            //Incrementamos num
+            num++;
+        }
+    }
+}
